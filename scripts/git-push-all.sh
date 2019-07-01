f96aa4b5b7c012943102ed1f336688c498b6e4e7
Abeeb <abibcode@gmail.com>
2019-07-01 17:54:46 +0100

    Updated_Registration_Service

--- scripts/git-push-all.sh
+++ scripts/git-push-all.sh
@@ -23,8 +23,6 @@ REPOSITORIES=($PREFIX $PREFIX.ApiGateway $PREFIX.Common $SERVICE.Identity $SERVI
       cd $REPOSITORY
       git add --all
       git commit -m $COMMIT_MESSAGE
-      #git remote add origin https://abibtj:$PASSWORD@bitbucket.org/abibtj/$REPOSITORY.git
-      git push https://abibtj:$PASSWORD@bitbucket.org/abibtj/$REPOSITORY.git --all 
-      #git push origin master
+      git push https://abibtj:$PASSWORD@bitbucket.org/abibtj/$REPOSITORY.git --all
       cd ..
     done
